a20931587a48c44372b11afd8db80cbc0087299a
sungwoongNoh <asdz453@gmail.com>
2024-08-11 21:26:04 +0900

    FEAT: repository단 테스트 코드 작성

--- src/test/java/com/lunch/lunch_recommend_api/domain/user/repo/UserRepositoryTest.java
+++ src/test/java/com/lunch/lunch_recommend_api/domain/user/repo/UserRepositoryTest.java
@@ -1,19 +1,23 @@
 package com.lunch.lunch_recommend_api.domain.user.repo;
 
+import com.lunch.lunch_recommend_api.conf.QueryDslConfig;
 import com.lunch.lunch_recommend_api.domain.user.UserEntity;
 import com.lunch.lunch_recommend_api.domain.user.dto.req.UserDtoReq;
-import org.assertj.core.api.Assertions;
+import com.lunch.lunch_recommend_api.domain.user.dto.res.UserDtoRes;
+import jakarta.persistence.SqlResultSetMapping;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class UserRepositoryImpl {
+@Import(QueryDslConfig.class)
+public class UserRepositoryTest {
 
     @Autowired
     UserRepository userRepository;
@@ -60,10 +64,30 @@ public class UserRepositoryImpl {
 
 
         //then
-        assertThatThrownBy(() -> {
-            userRepository.save(userEntity_2);
-        }).isInstanceOf(Exception.class);
-
+        assertThat(userRepository.findAll().size()).isEqualTo(1);
     }
 
+
+    @Test
+    @DisplayName("ID로 유저를 조회한다 - 성공")
+    public void findUserById_test() {
+
+        //given
+        UserDtoReq requestDto = UserDtoReq.builder()
+                .userId("test_user")
+                .name("tester")
+                .password("12345678")
+                .build();
+
+
+        //when
+        userRepository.save(requestDto.toEntity());
+
+        UserDtoRes userById = userRepository.findUserById(requestDto.getUserId());
+
+        //then
+        assertThat(userById.userId()).isEqualTo("test_user");
+        assertThat(userById.name()).isEqualTo("tester");
+
+    }
 }
